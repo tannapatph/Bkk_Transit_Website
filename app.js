9b4f10fff22a6a4c4b5acb66538b48bf1b98d35b
Tannapatph <tannapatph@gmail.com>
2025-11-16 23:38:46 +0700

    add backend URL

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ----- 0. Configuration -----
-const API_BASE_URL = "http://127.0.0.1:8000"; 
+const API_BASE_URL = "https://bkk-transit-website.onrender.com"; 
 
 // ----- 1. Global Variables -----
 let allStations = []; 
